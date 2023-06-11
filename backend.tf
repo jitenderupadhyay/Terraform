b30786f16da0aa6bae93c3ba4c59ca17404fd2e9
Jitender Upadhyay <j.upadhyay.niec@gmail.com>
2023-06-11 18:15:13 +0530

    added S3 and DynamoDb, backend for maintaining tf state in aws storage

---
+++ backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-practice-jeet"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-locks-jeet"
+    encrypt        = true
+  }
+}
--- main.tf
+++ main.tf
@@ -57,3 +57,40 @@ resource "aws_sqs_queue_policy" "my_sqs_policy" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-practice-jeet"
+ 
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks-jeet"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
